5963794da291a20fde64e29139f55f3b2a62110c
Melih Akyıldız <melihakyildiz0206@gmail.com>
2024-05-19 18:41:09 +0300

    Add project files.

---
+++ macTower.PresentationLayer/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿
+
+//ham menü func
+function hamMenuFunc() {
+    var x = document.querySelector(".ham-menu-Nav");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+    console.log("sss")
+}
+
+
+////opacity ayarı
+//document.addEventListener('DOMContentLoaded', (event) => {
+//    const dropdown = document.querySelector('.dropdown');
+//    const pageContent = document.querySelector('.page-content');
+
+//    dropdown.addEventListener('mouseover', () => {
+//        pageContent.style.opacity = '0.2';
+//    });
+
+//    dropdown.addEventListener('mouseout', () => {
+//        pageContent.style.opacity = '1';
+//    });
+//});
